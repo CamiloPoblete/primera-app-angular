778817df5d1f0e49367b6d8b544af3195cf2ed68
chamilo <camilo.martinez.191190@gmail.com>
2022-03-16 20:43:59 -0300

    se agrego otro proyecto

--- 01-09-CambiosComponenteAngular-UA/src/app/app.component.ts
+++ 01-09-CambiosComponenteAngular-UA/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mi primera App en Angular';
+  title = 'Mi Primer App en Angular';
 }
